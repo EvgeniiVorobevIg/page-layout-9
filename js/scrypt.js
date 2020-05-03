d17d2a10ddcafd3e61b98acf037395dfc282a5f7
wayoutl1 <63786156+wayoutl1@users.noreply.github.com>
2020-05-03 23:59:46 +0500

    add2013

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     $('.burger').click(function (event) {
         $('.burger,.footer-form,.circle').toggleClass('active');
-
-
     });
+    $('.spoiler__question').click(function (event) {
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
+});
+$('.block__slider').slick({
 });
